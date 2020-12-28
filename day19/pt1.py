6e226881877fcac7bb2bce2eb5419557766d17e5
Tom Fenech <tomjwfenech@gmail.com>
2020-12-28 08:01:02 +0100

    Day 19, part 1

---
+++ day19/pt1.py
@@ -0,0 +1,35 @@
+import sys
+import re
+
+
+def parse(lines):
+    return {a: b for a, b in map(lambda line: line.split(": "), lines)}
+
+
+def resolve(rules, rule="0"):
+    r = rules[rule]
+    if re.search(r"\d", r) is None:
+        return re.sub(r"[\" ]+", "", r)
+
+    r = re.sub(r"\d+", lambda m: "(" + resolve(rules, m.group(0)) + ")", r)
+    return r
+
+
+def stripper(lines):
+    for line in lines:
+        yield line.rstrip()
+
+
+def block(lines):
+    for line in lines:
+        if line == "":
+            break
+        yield line
+
+
+if __name__ == "__main__":
+    lines = stripper(sys.stdin)
+    rules = parse(block(lines))
+    matcher = re.compile("^" + resolve(rules).replace(" ", "") + "$")
+
+    print(sum(m is not None for m in map(matcher.match, lines)))
